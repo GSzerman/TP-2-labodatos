57fa889b65d3ddeeacbbb8e6a7bebfe4f2f7e838
Gabriel <gabriel.szerman@gmail.com>
2023-10-31 23:31:48 -0300

    Avanzado ejercicio de clasificacion con decision tree

--- Scripts/Gabi/Ejercicio 2.R
+++ Scripts/Gabi/Ejercicio 2.R
@@ -1,18 +1,96 @@
 install.packages("ggplot2")
-library(ggplot2)
+install.packages("rpart")
 
-# Crear un gráfico de dispersión con ggplot
-ggplot(fake_news, aes(x = negative, y = title_words, color = title_has_excl)) +
+library(ggplot2)
+library(rpart)
+
+# EJERCICIO 2 : Clasificación
+
+
+# - Exploración -
+
+
+
+# gráfico de dispersión "porc palabras negativas" vs "numero de palabras en el titulo"
+ggplot(fake_news, aes(x = negative, y = title_words, color = type)) +
   geom_point() +
   labs(x = "Porcentaje de palabras negativas",
        y = "Número de palabras en el título",
-       color = "Tiene signos de exclamación en el título") +
+       color = "Tipo") +
   ggtitle("Relación entre title_has_excl, negative y title_words")
 
 
 
+# gráfico de dispersión para "title_has_excl" vs "negative" respecto a "type"
+ggplot(fake_news, aes(x = title_has_excl, y = negative, color = type)) +
+  geom_point() +
+  labs(x = "Título con exclamación", y = "Porcentaje de connotaciones negativas") +
+  ggtitle("Gráfico de dispersión: title_has_excl vs negative por tipo de noticia")
 
-# Tomo de ejemplo los ejs de la guia de decision tree
+# gráfico de dispersión para "title_has_excl" vs "title_words" respecto a "type"
+ggplot(fake_news, aes(x = title_has_excl, y = title_words, color = type)) +
+  geom_point() +
+  labs(x = "Título con exclamación", y = "Número de palabras en el título") +
+  ggtitle("Gráfico de dispersión: title_has_excl vs title_words por tipo de noticia")
+
+
+
+# - Decision Tree -
+
+# Establecemos una semilla para la reproducibilidad
+set.seed(123)
+
+# Creamos un vector de índices para el conjunto de entrenamiento
+n_train <- sample(1:nrow(fake_news), replace = FALSE, 0.8 * nrow(fake_news))
+
+# Creamos los conjuntos de entrenamiento y prueba
+train_data <- fake_news[n_train, ]
+test_data <- fake_news[-n_train, ]
+
+
+
+
+
+
+# Construimos el clasificador de árbol de decisión
+tree_model1 <- rpart(type ~ title_has_excl + negative + title_words, data = train_data)
+
+# Pendiente: Ir desglosando entre las distintas combinaciones de variables
+
+# Predecimos las clases en el conjunto de prueba
+tree_predictions <- predict(tree_model1, test_data, type = "class")
+
+# Calculamos la matriz de confusión
+confusion_matrix_tree <- table(Real = test_data$type, Predicho = tree_predictions)
+
+# Calculamos la precisión del modelo
+accuracy_tree <- sum(diag(confusion_matrix_tree)) / sum(confusion_matrix_tree)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################ Borrador ################
+
+# Tomo de ejemplo los ejs de la guia de decision tree # 
+
+
 
 pens <- penguins %>%
   filter(sex != is.na(sex))
